89fdad1004b2a4d0bb2f8b7eae85591af77697b1
alex-0914 <alextangting@gmail.com>
2020-10-13 14:31:34 +0800

    project structure done

---
+++ iam/frontend/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component }from 'react';
+import Router from 'react-router';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+  
+    )
+  }
+}
+export default App;
